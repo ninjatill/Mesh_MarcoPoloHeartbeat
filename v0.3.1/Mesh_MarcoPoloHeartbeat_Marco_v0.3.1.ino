0f67c57a094fcd3071991ed50be03daca8b5f20c
ninjatill <ninjatill@yahoo.com>
2018-12-14 14:00:03 +0000

    Fixed miss-posting of polo code to marco. Oops!

--- v0.3.1/Mesh_MarcoPoloHeartbeat_Marco_v0.3.1.ino
+++ v0.3.1/Mesh_MarcoPoloHeartbeat_Marco_v0.3.1.ino
@@ -1,66 +1,120 @@
 #include "Particle.h"
 
+//SYSTEM_THREAD(ENABLED);
 
-SYSTEM_THREAD(ENABLED);
+#define MAX_MESH_NODES 10  //Maximum number of nodes expected on the mesh. Make small as possible to preserve memory space.
 
+enum stats { TotalHeartbeats = 0, ResponsesReceived = 1 };
 
 bool heartbeat = false;
-bool meshPub = false;
+bool cloudPub = false;
 
-unsigned long lastBeatTime = 0;
-unsigned long beatTimeout = 1000;
+unsigned long beatInterval = 10000;
+unsigned long lastBeatMillis = 0;
+unsigned long beatTimeout = 8000;
+unsigned long lastPoloMillis = 0;
 
-bool meshLost = false;
-unsigned long meshLostTime = 0;
-unsigned long meshResetTimeout = 600000;  //10 min = 600000
+//Variables to record known and reporting endpoints, or nodes.
+uint8_t knownNodesCount = 0;
+char knownNodes[MAX_MESH_NODES][32];
+unsigned long knownNodesStats[MAX_MESH_NODES][2];  //Use to calculate response percentages. Use enum stats to acceess second dimension.
 
-const char version[] = "MeshMarcoPoloHeartbeat_Polo 0.3.1";
+uint8_t reportingNodesCount = 0;
+char reportingNodes[MAX_MESH_NODES][32];
+unsigned long reportingNodesMillis[MAX_MESH_NODES];
+
+//Variables to record when cloud connection is lost.
+bool cloudLost = false;
+unsigned long cloudLostMillis = 0;
+unsigned long cloudResetTimeout = 600000; //10 min = 600000
+
+unsigned long beatsSent = 0;
+
+
+const char version[] = "Mesh_MarcoPoloHeartbeat_Marco_v0.3.1";
 
 
 void setup() {
     Serial.begin(9600);
     
     Particle.variable("version", version);
-
+    Particle.publish("Marco-Polo heartbeat test started.");
+    
     pinMode(D7, OUTPUT);
     pinMode(D4, INPUT_PULLDOWN);
     
     if (digitalRead(D4) == HIGH) {
         SelectExternalMeshAntenna();
     }
+
+    Mesh.subscribe("Polo", ProcessBeat);
     
-    Mesh.subscribe("Marco", ProcessBeat);
+    ResetReportingNodes();
+    
+    Serial.printlnf("Version: %s", version);
+    Serial.println("Marco-Polo heartbeat test started.");
 }
 
 
 void loop() {
-    
-    if (heartbeat && meshPub) {
+
+    //Send heartbeat collection message every beatInterval.
+    if (!heartbeat && ((millis() - lastBeatMillis) >= beatInterval)) {
+        Serial.printlnf("Heartbeat started: %dms response window.", beatTimeout);
+        ResetReportingNodes();
+        
+        heartbeat = true;
+        lastBeatMillis = millis();
+        lastPoloMillis = lastBeatMillis;
+        digitalWrite(D7, HIGH);
         if (Mesh.ready()) {
-            Mesh.publish("Polo", System.deviceID());
-            meshPub = false;
-            meshLost = false;
+            Serial.println("Mesh Available. sending Marco...");
+            Mesh.publish("Marco");
         } else {
-            if (!meshLost) {
-                meshLostTime = millis();
-            }
-            meshPub = false;
-            meshLost = true;
+            Serial.println("Mesh not ready. Cannot send Marco...");
+            heartbeat = false;
         }
     }
     
     //Turn off LED after beat timeout.
-    if(heartbeat && ((millis() - lastBeatTime) >= beatTimeout)) {
+    if(heartbeat && ((millis() - lastBeatMillis) >= beatTimeout)) {
         heartbeat = false;
+        cloudPub = true;
         digitalWrite(D7, LOW);
+
+        Serial.println("Marco response period timed out.");
+        ResponseWrapUp();
     }
     
-    //Reset if mesh network is down longer than meshResetTimeout (default 10 min).
-    if (meshLost) {
-        if (Mesh.ready()) {
-            meshLost = false;
+    //Publish collected heartbeat results to cloud.
+    if(cloudPub) {
+        if (Particle.connected()) {
+            Serial.print("Publishing results to cloud: ");
+            
+            char msg[80];
+            snprintf(msg, arraySize(msg)-1, "Nodes:%d of %d;Millis:%d", reportingNodesCount, knownNodesCount, lastPoloMillis - lastBeatMillis);
+            Serial.println(msg);
+            
+            bool pubSuccess = Particle.publish("MarcoPoloHeartbeat", msg, PRIVATE);
+            if (!pubSuccess) { StartCloudLost(); }
+            
+            cloudPub = false;
+            
+            //TODO: Report which knownNodes did not report.
+        }
+        else {
+            StartCloudLost();
+        }
+    }
+    
+    //Check for lost cloud. Reset if down for more than cloudResetTimeout (default 10 min).
+    if (cloudLost) {
+        if (Particle.connected()) {
+            EndCloudLost();
         } else {
-            if (millis() - meshLostTime > meshResetTimeout) {
+            if (millis() - cloudLostMillis > cloudResetTimeout) {
+                Serial.println("Cloud did not re-establish before timeout. Resetting device...");
+                delay(500);
                 System.reset();
             }
         }
@@ -69,10 +123,66 @@ void loop() {
 
 
 void ProcessBeat(const char *name, const char *data) {
-    heartbeat = true;
-    meshPub = true;
-    lastBeatTime = millis();
-    digitalWrite(D7,HIGH);
+    //Record the device that is responding and exit fast.
+    uint8_t count = reportingNodesCount++;
+    snprintf(reportingNodes[count], arraySize(reportingNodes[count])-1, data);
+    reportingNodesMillis[count] = millis();
+    lastPoloMillis = millis();
+}
+
+void ResetReportingNodes() {
+    for (int i; i < arraySize(reportingNodes); i++) {
+        snprintf(reportingNodes[i], arraySize(reportingNodes[i])-1, "");
+    }
+    
+    reportingNodesCount = 0;
+}
+
+void StartCloudLost() {
+    if (!cloudLost) {
+        cloudLostMillis = millis();
+        Serial.println("Cloud lost. Starting timer.");
+    }
+    cloudPub = false;
+    cloudLost = true;
+}
+
+void EndCloudLost() {
+    cloudLost = false;
+    Serial.println("Cloud Restored. Disabling timer.");
+}
+
+void ResponseWrapUp() {
+    Serial.println("Cataloging responses.");
+    //Loop through reporting nodes and catatalog any unknown nodes.
+    for (int x = 0; x < arraySize(reportingNodes); x++) {
+        if (x < reportingNodesCount) {
+            Serial.printlnf("Response. index: %i; device: %s; millis: %u", x, reportingNodes[x], reportingNodesMillis[x] - lastBeatMillis);
+        } else {
+            break;
+        }
+        
+        for (int i = 0; i < arraySize(knownNodes); i++) {
+            //If we get to a blank array slot, record this node there.
+            if (strcmp(knownNodes[i],"") == 0) {
+                snprintf(knownNodes[i], arraySize(knownNodes[i])-1, reportingNodes[x]);
+                knownNodesCount++;
+                knownNodesStats[i][ResponsesReceived]++;
+                break;
+            }
+            
+            //If we the reporting know is known already stop looking for this reporting Node.
+            if (strcmp(knownNodes[i],reportingNodes[x]) == 0) {
+                knownNodesStats[i][ResponsesReceived]++;
+                break;
+            } 
+        }
+    }
+    
+    //Increment the number of total heartbeats.
+    for (int i; i < knownNodesCount; i++) {
+        knownNodesStats[i][TotalHeartbeats]++;
+    }
 }
 
 void SelectExternalMeshAntenna() {
@@ -86,4 +196,3 @@ void SelectExternalMeshAntenna() {
     	digitalWrite(ANTSW2, 1);
     #endif
 }
-
